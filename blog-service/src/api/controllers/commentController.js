f440804498e2a606bf9d74feb9ccaa3028460e7b
pili01 <pilipovicdusko002@gmail.com>
2025-08-14 03:02:27 +0200

    feat: check user authorization

--- blog-service/src/api/controllers/commentController.js
+++ blog-service/src/api/controllers/commentController.js
@@ -1,8 +1,24 @@
 const commentService = require('../../service/commentService');
+const authService = require('../../service/authService');
 
 exports.createComment = async (req, res, next) => {
     try {
+        const data = await authService.getMe(req.headers.authorization);
+        if (!data || !data.role || !data.userId) {
+            return res.status(403).json({
+                success: false,
+                message: "Forbidden"
+            });
+        }
+
+        if (data.role != "Tourist" && data.role != "Guide") {
+            return res.status(403).json({
+                success: false,
+                message: "Forbidden"
+            });
+        }
         const commentData = req.body;
+        commentData.userId = data.userId;
         const savedComment = await commentService.createComment(commentData);
         res.status(201).json({
             success: true,
